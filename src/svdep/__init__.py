c799cf5361d35d6b25c73955e29982928b5bc1e7
Matthew Ballance <matt.ballance@gmail.com>
2026-01-31 01:58:23 +0000

    Add python hash utilities
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/svdep/__init__.py
+++ src/svdep/__init__.py
@@ -1,6 +1,7 @@
 
 from .file_collection import FileCollection
 from .native import is_native_available, get_native_library_path
+from .hash_files import compute_hash_for_files
 
 # Import pure-Python implementations
 from .task_check_up_to_date import TaskCheckUpToDate as _PythonTaskCheckUpToDate
